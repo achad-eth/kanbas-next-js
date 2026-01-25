e14b1cc84a17fcf9912055133d3f136f18cd45fb
hellwise1 <arynchadha@gmail.com>
2026-01-25 15:45:05 -0500

    Final Push+1

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Web Dev</h1>
-      <Link href="/labs">Labs</Link>
-    </div>
-  );
+  redirect("/account/signin");
 }
-
